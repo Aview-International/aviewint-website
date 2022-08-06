e6cee0423b8e25062becabe8e46d20ff846d2403
Andrew Qiao <andrewqiao2004@gmail.com>
2022-08-06 15:55:22 -0400

    Revert "Testing form validation using React Hook Form and Yup"
    This reverts commit 58dd47220412d78b47015202f1f0a28cb27db5ae.

--- components/sections/home/Section1.js
+++ components/sections/home/Section1.js
@@ -7,9 +7,6 @@ import Image from 'next/image';
 import { MILESTONES } from '../../../constants/constants';
 import { useState, useRef, useEffect } from 'react';
 import { useOnScreen } from '../../../hooks/useOnScreen';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
 
 const Section1 = () => {
   return (
@@ -37,67 +34,15 @@ const Section1 = () => {
 };
 
 const PhoneNumberForm = () => {
-  const phoneRegExp =
-    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-
-  const validateSchema = Yup.object().shape({
-    phoneNumber: Yup.string().matches(
-      phoneRegExp,
-      'Enter a valid phone number.'
-    ),
-  });
-
-  const onSubmit = async (data) => {
-    try {
-      const response = await fetch('/', {
-        method: 'POST',
-        body: JSON.stringify({
-          query: data,
-        }),
-      });
-      console.log(response, 'Form submitted successfully');
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setValue('phoneNumber', '');
-    }
-  };
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm({
-    mode: 'onBlur',
-    resolver: yupResolver(validateSchema),
-  });
-
   return (
     <form
       name="phone-number"
       method="POST"
-      action="#"
-      onSubmit={handleSubmit(onSubmit)}
       data-netlify="true"
       className="lg:flex lg:justify-between lg:gap-s2"
     >
       <div className="mb-s2 lg:mb-0 lg:grow">
-        <div className="w-full">
-          <Shadow classes="w-full">
-            <Border borderRadius="md" classes="w-full">
-              <input
-                type="tel"
-                {...register('phoneNumber')}
-                id="phoneNumber"
-                placeholder="Phone Number"
-                className="w-full rounded-md bg-black px-s2 py-2 text-lg text-white placeholder:font-light placeholder:text-white focus:outline-none md:text-xl"
-              />
-            </Border>
-          </Shadow>
-          <p className="text-red">{errors.phoneNumber?.message}</p>
-        </div>
-        {/* <TextInput type="tel" name="phone-number" placeholder="Phone Number" /> */}
+        <TextInput type="tel" name="phone-number" placeholder="Phone Number" />
       </div>
       <Button type="primary" purpose="submit">
         Get Started
