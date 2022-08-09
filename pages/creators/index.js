439c14b3f6acaeeca366b11bb5530e20db6d94e9
Andrew Qiao <andrewqiao2004@gmail.com>
2022-08-09 09:55:31 -0400

    Start working on creators page

---
+++ pages/creators/index.js
@@ -0,0 +1,15 @@
+import Header from '../../components/navigation/Header';
+import OurServices from '../../components/sections/reused/OurServices';
+import Footer from '../../components/navigation/Footer';
+
+const Creators = () => {
+  return (
+    <>
+      <Header />
+      <OurServices />
+      <Footer />
+    </>
+  );
+};
+
+export default Creators;
