71e66f5569a248caafefca7a8f93d57b055bb068
Taehun Jang <taehunjang93@gmail.com>
2022-08-11 18:19:25 -0500

    update translator page

--- components/sections/translator/CompanyCulture.js
+++ components/sections/translator/CompanyCulture.js
@@ -1,5 +1,56 @@
-const CompanyCulture = () => {
+import GraphicCard from '../../UI/GraphicCard';
+import Image from 'next/image';
+import ed2Focus from '../../../public/img/graphics/ed2.png';
+import collaborativeWork from '../../../public/img/graphics/collabo.png';
+import personalGrowth from '../../../public/img/graphics/personal.png';
+import teambuilding from '../../../public/img/graphics/team-building.png';
 
+
+const CARD_CONTENT = [
+  {
+    title: 'Teambuilding',
+    graphic: teambuilding,
+  },
+  {
+    title: 'Collaborative Work',
+    graphic: collaborativeWork,
+  },
+  {
+    title: 'Personal Growth',
+    graphic: personalGrowth,
+  },
+  {
+    title: 'EDII Focus',
+    graphic: ed2Focus,
+  },
+];
+
+const CompanyCulture = () => {
+  return (
+    <section className="section m-horizontal text-center">
+      <h2 className="title mb-s4 md:mb-s10">
+        Benefits of{' '}
+        <span className="gradient-text gradient-2">Translations</span>
+      </h2>
+      <CardGrid />
+    </section>
+  );
+};
+
+const CardGrid = () => {
+  return (
+    <div className="mx-auto grid max-w-[1030px] gap-x-s2.5 gap-y-s4 md:grid-cols-2">
+      {CARD_CONTENT.map((list) => (
+        <GraphicCard
+          key={list.id}
+          title={list.title}
+          description={list.description}
+          graphic={list.graphic}
+          direction="vertical"
+        />
+      ))}
+    </div>
+  );
   }
   
   export default CompanyCulture;
--- components/sections/translator/MakeImpact.js
+++ components/sections/translator/MakeImpact.js
@@ -1,5 +1,20 @@
-const MakeImpact = () => {
+import FullWidthGradient from '../../layout/FullWidthGradient';
 
+const MakeImpact = () => {
+  return (
+    <section className="section">
+      <FullWidthGradient>
+
+        <h2 className="title mx-auto max-w-[1243px] text-center">
+        Make Your Impact Today!
+        </h2>
+        <p className="body mx-auto mb-s2 max-w-[810px] text-center">
+        Aview works with the largest influencers in the world. 
+        Our translations are seen by millions of international fans.
+        </p>
+      </FullWidthGradient>
+    </section>
+  );
   }
   
   export default MakeImpact;
