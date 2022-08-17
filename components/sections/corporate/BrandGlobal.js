e52c4c43b614f3c3c021db6e45552e62f646daf9
Taehun Jang <taehunjang93@gmail.com>
2022-08-17 11:38:01 -0500

    Update BrandGlobal.js

--- components/sections/corporate/BrandGlobal.js
+++ components/sections/corporate/BrandGlobal.js
@@ -23,16 +23,16 @@ const BrandGlobal = () => {
            Contact Us
           </Button>
         </div>
-        <div className="mt-s4 lg:col-span-3 min-w-[300px] lg:min-w-[400px] lg:my-auto">
+        <div className="mt-s4 lg:col-span-4 max-w-[600px] lg:max-w-[40vw]">
           <Image src={BrandGlobalImage} alt="landing-graphic" />
           </div>
-          <span className='absolute w-[167px] right-80 top-28'>
+          <span className='absolute w-[12vw] min-w-[90px] max-w-[167px] lg:right-52 lg:top-28 md:right-56 md:-bottom-20 right-32 -bottom-12'>
           <Image src={BrandIcon1} alt="" />
         </span>
-        <span className='absolute w-[196px] right-28 top-48'>
+        <span className='absolute w-[14vw] min-w-[106px] max-w-[196px] lg:right-4 lg:top-48 md:right-28 md:-bottom-44 right-12 -bottom-36'>
           <Image src={BrandIcon2} alt="" />
         </span>
-        <span className='absolute w-[147px] right-24 -bottom-40'>
+        <span className='absolute w-[10vw] min-w-[80px] max-w-[147px] lg:right-4 lg:top-96 md:right-24 md:-bottom-72 right-12 -bottom-60'>
           <Image src={BrandIcon3} alt="" />
         </span>
       </div>
