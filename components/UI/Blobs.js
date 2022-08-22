1f85b8f0f0e8fee328ca17971e96be940fe5611c
Andrew Qiao <andrewqiao2004@gmail.com>
2022-08-22 17:06:06 -0400

    Hide blobs on menu open

--- components/UI/Blobs.js
+++ components/UI/Blobs.js
@@ -1,22 +1,42 @@
 import Image from 'next/image';
+import { useContext } from 'react';
 import blob1 from '../../public/img/blobs/blob-1.png';
 import blob2 from '../../public/img/blobs/blob-2.png';
 import blob3 from '../../public/img/blobs/blob-3.png';
 import blob4 from '../../public/img/blobs/blob-4.png';
+import MenuOpenContext from '../../store/menu-open-context';
 
 const Blobs = () => {
+  const menuOpenCtx = useContext(MenuOpenContext);
+
   return (
     <>
-      <div className="absolute -top-[5%] -right-[5%] -z-30 w-[70vw]">
+      <div
+        className={`absolute -top-[5%] -right-[5%] -z-30 w-[70vw] ${
+          menuOpenCtx.isMenuOpen && 'hidden'
+        }`}
+      >
         <Image src={blob1} alt="blob2" />
       </div>
-      <div className="absolute top-[80vh] -left-[30vw] z-30 w-[70vw]">
+      <div
+        className={`absolute top-[80vh] -left-[30vw] z-30 w-[70vw] ${
+          menuOpenCtx.isMenuOpen && 'hidden'
+        }`}
+      >
         <Image src={blob2} alt="blob2" />
       </div>
-      <div className="absolute top-[200vh] -right-[30vw] -z-30 w-[90vw]">
+      <div
+        className={`absolute top-[200vh] -right-[30vw] -z-30 w-[90vw] ${
+          menuOpenCtx.isMenuOpen && 'hidden'
+        }`}
+      >
         <Image src={blob3} alt="blob3" />
       </div>
-      <div className="absolute top-[250vh] -left-[10vw] -z-30 w-[80vw]">
+      <div
+        className={`absolute top-[250vh] -left-[10vw] -z-30 w-[80vw] ${
+          menuOpenCtx.isMenuOpen && 'hidden'
+        }`}
+      >
         <Image src={blob4} alt="blob4" />
       </div>
     </>
