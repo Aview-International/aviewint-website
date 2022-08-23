fb140844c17f6d6fe8dedfa5ec5ef0d36c9cad14
Andrew Qiao <andrewqiao2004@gmail.com>
2022-08-23 14:56:05 -0400

    Copywriting adjustments

--- constants/constants.js
+++ constants/constants.js
@@ -71,10 +71,9 @@ export const ROUTES = [
   { id: 'route-1', text: 'Home', route: '/' },
   { id: 'route-2', text: 'Creators', route: '/creators' },
   { id: 'route-3', text: 'Corporate', route: '/corporate' },
-  { id: 'route-4', text: 'Bill C-96', route: '/bill-c96' },
-  { id: 'route-5', text: 'About', route: '/about' },
-  { id: 'route-6', text: 'Careers', route: '/careers' },
-  { id: 'route-7', text: 'Blog', route: '/blog' },
+  { id: 'route-4', text: 'About', route: '/about' },
+  { id: 'route-5', text: 'Careers', route: '/careers' },
+  { id: 'route-6', text: 'Blog', route: '/blog' },
 ];
 
 export const MILESTONES = [
