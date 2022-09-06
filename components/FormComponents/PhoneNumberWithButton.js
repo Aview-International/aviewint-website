457704156f782b7a77219b4edbf6e0c407c4287a
victor <vicopem@gmail.com>
2022-09-06 23:12:50 +0100

    Phone number component on creators page

---
+++ components/FormComponents/PhoneNumberWithButton.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { initiateBot } from '../../utils/submit-form';
+import Button from '../UI/Button';
+import PhoneNumberInput from './PhoneNumberInput';
+
+const PhoneNumberWithButton = ({ setShowText }) => {
+  const [phone, setPhone] = useState({
+    phone: '',
+    hasSubmitted: false,
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPhone({ ...phone, hasSubmitted: true });
+    if (phone.phone.length < 11 || phone.phone.length > 18) return;
+    try {
+      initiateBot([
+        {
+          phoneNumber: phone.phone,
+          payLoad: Date.now(),
+        },
+      ]);
+      setShowText(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <form
+      className="lg:flex lg:justify-between lg:gap-s2"
+      onSubmit={handleSubmit}
+    >
+      <div className="mb-s2 lg:mb-0 lg:grow">
+        <PhoneNumberInput
+          onChange={(e) => setPhone({ ...phone, phone: e })}
+          value={phone.phoner}
+          hasSubmitted={phone.hasSubmitted}
+          isValid={phone.phone?.length > 11 && phone.phone?.length < 18}
+        />
+      </div>
+      <Button type="primary" purpose="submit">
+        Get Started
+      </Button>
+    </form>
+  );
+};
+export default PhoneNumberWithButton;
--- components/sections/creators/WorkWithUs.js
+++ components/sections/creators/WorkWithUs.js
@@ -1,13 +1,8 @@
 import { useState } from 'react';
-import PhoneNumberInput from '../../FormComponents/PhoneNumberInput';
-import Button from '../../UI/Button';
+import PhoneNumberWithButton from '../../FormComponents/PhoneNumberWithButton';
 
 const WorkWithUs = () => {
   const [showText, setShowText] = useState(false);
-  const [phone, setPhone] = useState({
-    phone: '',
-    hasSubmitted: false,
-  });
   return (
     <section className="m-horizontal section w-full md:w-[60%]">
       <h2 className="title">
@@ -17,47 +12,8 @@ const WorkWithUs = () => {
         At AVIEW, we work with all sizes of content creators. Leave your number
         and our team will be in touch
       </p>
-      <PhoneNumberForm
-        setPhone={setPhone}
-        phone={phone}
-        setShowText={setShowText}
-      />
+      <PhoneNumberWithButton setShowText={setShowText} />
     </section>
-  );
-};
-
-const PhoneNumberForm = ({ setPhone, phone, setShowText }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setPhone({ ...phone, hasSubmitted: true });
-    if (phone.phone.length < 11 || phone.phone.length > 18) return;
-    try {
-      initiateBot([
-        {
-          phoneNumber: phone.phone,
-          payLoad: Date.now(),
-        },
-      ]);
-      setShowText(true);
-    } catch (error) {}
-  };
-  return (
-    <form
-      className="lg:flex lg:justify-between lg:gap-s2"
-      onSubmit={handleSubmit}
-    >
-      <div className="mb-s2 lg:mb-0 lg:grow">
-        <PhoneNumberInput
-          onChange={(e) => setPhone({ ...phone, phone: e })}
-          value={phone.phoner}
-          hasSubmitted={phone.hasSubmitted}
-          isValid={phone.phone?.length > 11 && phone.phone?.length < 18}
-        />
-      </div>
-      <Button type="primary" purpose="submit">
-        Get Started
-      </Button>
-    </form>
   );
 };
 
