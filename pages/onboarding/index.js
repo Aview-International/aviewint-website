0ca2c53aa4e2c3542a1acde8a2ef644644e7a0c9
victor <vicopem@gmail.com>
2022-10-04 01:44:13 +0100

    Onboarding SEO component added

--- pages/onboarding/index.js
+++ pages/onboarding/index.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import Header from '../../components/navigation/Header';
 import {
   OnboardingStep1,
   OnboardingStep2,
