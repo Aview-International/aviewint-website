d0c5217b338d69326750b91ca01119442303af53
Andrew Qiao <andrewqiao2004@gmail.com>
2022-10-20 14:45:50 -0400

    Change color of table of contents

--- components/blogs/TableOfContents.js
+++ components/blogs/TableOfContents.js
@@ -3,16 +3,18 @@ import Link from 'next/link';
 const TableOfContents = ({ sections }) => {
   return (
     <div className="mb-s5">
-      <h4 className="mb-s2 text-xl font-bold text-white">Skip to a section</h4>
-      <ul className="text-white">
+      <h4 className="mb-s2 text-xl font-bold text-white md:text-3xl">
+        Skip to a section
+      </h4>
+      <ul>
         {sections.map((section, i) => (
           <li
             className="mb-s2 grid grid-cols-[auto_auto] justify-start gap-3"
             key={`section-${i}`}
           >
-            <div className="mt-1.5 h-2 w-2 rounded-full bg-white"></div>
+            <div className="mt-1.5 h-2 w-2 rounded-full bg-blue"></div>
             <Link href={section.link}>
-              <a className="gradient-text gradient-1 text-lg">
+              <a className="text-lg text-blue underline md:text-xl">
                 {section.title}
               </a>
             </Link>
