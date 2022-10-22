95dc42661732b2f7f6e27388d2d226dd28b73c51
Andrew Qiao <andrewqiao2004@gmail.com>
2022-10-22 17:44:59 -0400

    Revert "Update copy on landing page"
    This reverts commit 2984cd28d1196d5b6e2261e90bd4a6443fea4848.

--- components/sections/home/BreadAndButter.js
+++ components/sections/home/BreadAndButter.js
@@ -4,14 +4,12 @@ const BreadAndButter = () => {
   return (
     <section className="section">
       <FullWidthGradient>
-        <h2 className="title mx-auto mb-4 max-w-[640px] text-center">
+        <h2 className="title mx-auto mb-4 max-w-[593px] text-center">
           Translating Content, It&apos;s Our Bread and Butter
         </h2>
-        <p className="body mx-auto max-w-[1243px] text-center">
-          We connect creators with world-class translators to help them build a
-          global audience. The value we create speaks for itself. With over
-          500M+ views, we&apos;ve helped creators and media companies meet their
-          revenue and audience growth goals.
+        <p className="body mx-auto max-w-[640px] text-center">
+          We know how to help expand our creators global fanbase. The clients we
+          work with speak for itself.
         </p>
       </FullWidthGradient>
     </section>
