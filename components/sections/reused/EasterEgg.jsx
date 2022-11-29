c928cf246045094563f07bdbb47687f95c1bbf0e
Andrew Qiao <andrewqiao2004@gmail.com>
2022-11-29 08:52:29 -0500

    Add marquee component

--- components/sections/reused/EasterEgg.jsx
+++ components/sections/reused/EasterEgg.jsx
@@ -1,7 +1,45 @@
+import Link from 'next/link';
+import Marquee from 'react-fast-marquee';
+
 export default function EasterEgg() {
   return (
-    <div className="gradient-2 py-3">
-      <p className="text-white md:text-lg">Click here to learn more</p>
-    </div>
+    <Link href="/coming-soon">
+      <div className="gradient-2 cursor-pointer py-3">
+        <Marquee gradient={false} speed={40}>
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            Click here to learn more
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            COMING SOON
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            Click here to learn more
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            COMING SOON
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            Click here to learn more
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            COMING SOON
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            Click here to learn more
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+          <p className="mr-s2 text-lg font-medium text-white md:mr-s4 md:text-xl">
+            COMING SOON
+          </p>
+          <div className="mr-s2 -mt-1 h-s1 w-s1 rounded-full bg-white md:mr-s4" />
+        </Marquee>
+      </div>
+    </Link>
   );
 }
