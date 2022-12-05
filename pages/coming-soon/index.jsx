8b0928f050ace05ad77a274bf5181b5da543ebee
victor <vicopem@gmail.com>
2022-12-06 00:28:06 +0100

    Connected form to netlify

--- pages/coming-soon/index.jsx
+++ pages/coming-soon/index.jsx
@@ -1,15 +1,33 @@
 import Image from 'next/image';
 import { useState } from 'react';
+import Form from '../../components/FormComponents/Form';
 import FormInput from '../../components/FormComponents/FormInput';
 import Header from '../../components/navigation/Header';
 import Button from '../../components/UI/Button';
 import comingSoon from '../../public/img/graphics/coming-soon.png';
+import { emailValidator } from '../../utils/regex';
+import { submitForm } from '../../utils/submit-form';
 
 export default function ComingSoon() {
-  const [email, setEmail] = useState('');
+  const [mail, setMail] = useState({
+    email: '',
+    hasSubmitted: false,
+  });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      setMail({ ...mail, hasSubmitted: true });
+      if (!emailValidator(mail.email)) return;
+      submitForm('newsletter', { email: mail.email });
+      router.push('/success');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   function emailChangeHandler(event) {
-    setEmail(event.target.value);
+    setMail({ ...mail, email: event.target.value });
   }
 
   return (
@@ -23,17 +41,26 @@ export default function ComingSoon() {
         <p className="body mb-s4">
           In the meantime, sign up for our newsletter to receive updates.
         </p>
-        <div className="mx-auto flex flex-col gap-s5 lg:max-w-[580px] lg:flex-row lg:items-center lg:gap-s2">
+        <Form
+          name="newsletter"
+          submitHandler={handleSubmit}
+          className="mx-auto flex flex-col gap-s5 lg:max-w-[580px] lg:flex-row lg:items-center lg:gap-s2"
+        >
           <FormInput
             placeholder="Your Email Address"
             onChange={emailChangeHandler}
-            value={email}
+            value={mail.email}
+            hasSubmitted={mail.hasSubmitted}
+            isValid={emailValidator(mail.email)}
             noBottomMargin={true}
+            name="email"
           />
           <div className="mt-s1">
-            <Button type="primary">Sign Up</Button>
+            <Button type="primary" purpose="submit">
+              Sign Up
+            </Button>
           </div>
-        </div>
+        </Form>
       </div>
       <div className="absolute top-0 left-1/2 -z-10 w-[250%] -translate-x-1/2 xs:w-[225%] sm:w-[200%] lg:w-full">
         <Image src={comingSoon} alt="easter egg graphic" />
