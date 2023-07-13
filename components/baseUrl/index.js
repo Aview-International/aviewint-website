d575c9307663d2ca9e57a4cca9f15e1beee6198f
victor <vicopem@gmail.com>
2023-07-13 18:30:05 -0400

    Updates: Changed base url

--- components/baseUrl/index.js
+++ components/baseUrl/index.js
@@ -1,5 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === 'development'
-    ? // ? 'http://localhost:4000/'
-      'http://aview.us-east-1.elasticbeanstalk.com/'
-    : 'http://aview.us-east-1.elasticbeanstalk.com/';
+    ? 'http://localhost:4000/'
+    : 'https://api.aviewint.com/';
