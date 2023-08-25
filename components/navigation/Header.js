7157754beb3fcd14d3608ab2dccd88c726f23a82
victor <vicopem@gmail.com>
2023-08-25 15:48:00 -0400

    Feat: Login button

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -40,8 +40,8 @@ const HeaderButtons = () => {
       <Button purpose="route" route="/#generate-aview" type="primary">
         Contact Us
       </Button>
-      <Button purpose="route" route="/waitlist" type="secondary">
-        Join Waitlist
+      <Button purpose="route" route="/login" type="secondary">
+       Login
       </Button>
     </div>
   );
