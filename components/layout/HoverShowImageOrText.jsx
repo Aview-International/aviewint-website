33c038eb4250fb06300708d51ec19ea7a152f764
victor <vicopem@gmail.com>
2023-08-25 14:12:32 -0400

    Fix: UI text color fix

--- components/layout/HoverShowImageOrText.jsx
+++ components/layout/HoverShowImageOrText.jsx
@@ -6,60 +6,71 @@ export default function HoverShowImageOrText({ items, borderStyle }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className={`grid gap-4 md:grid-cols-2 lg:grid-cols-3 ${borderStyle&&'gap-12'}`}>
-     { borderStyle ? 
-     <>
-      {items.map((item)=>{
-        return (
-          <div key={item.title}>
-            <div className={`h-[380px] group rounded-2xl cursor-pointer transition-transform duration-300 bg-white-transparent flex flex-col justify-center items-center gap-y-6 ${isHovered ? 'py-s2' : 'py-s4'}`} 
-             onMouseEnter={() => setIsHovered(true)}>
-              <div className="mx-auto">
-                <Image src={item.image} alt={item.title} />
+    <div
+      className={`grid gap-4 md:grid-cols-2 lg:grid-cols-3 ${
+        borderStyle && 'gap-12'
+      }`}
+    >
+      {borderStyle ? (
+        <>
+          {items.map((item) => {
+            return (
+              <div key={item.title}>
+                <div
+                  className={`group flex h-[380px] cursor-pointer flex-col items-center justify-center gap-y-6 rounded-2xl bg-white-transparent transition-transform duration-300 ${
+                    isHovered ? 'py-s2' : 'py-s4'
+                  }`}
+                  onMouseEnter={() => setIsHovered(true)}
+                >
+                  <div className="mx-auto">
+                    <Image src={item.image} alt={item.title} />
+                  </div>
+                  <p className={`text-center text-3xl font-bold text-white`}>
+                    {item.title}
+                  </p>
+                  <a
+                    className={`block cursor-pointer text-center italic text-white/80 underline duration-300 ${
+                      isHovered && 'group-hover:hidden'
+                    }`}
+                    onMouseLeave={() => setIsHovered(false)}
+                  >
+                    Learn more
+                  </a>
+                  <p
+                    className={`hidden px-5 text-lg duration-300 ${
+                      isHovered && 'opacity-100 group-hover:block'
+                    }`}
+                  >
+                    {item.description}
+                  </p>
+                </div>
               </div>
-              <p className={`text-3xl font-bold text-white text-center`}>
-                {item.title}
-              </p>
-              <a className={`italic cursor-pointer text-center underline duration-300 text-white/80 block ${isHovered&& 'group-hover:hidden'}`} 
-               onMouseLeave={() => setIsHovered(false)}
-              >
-                Learn more
-              </a>
-              <p className={`hidden px-5 duration-300 text-lg ${isHovered&&'group-hover:block opacity-100'}`}>{item.description}</p>
-            </div>
-          </div>
-        )
-      })}
-     </>
-     :
-     <>
-     {
-      items.map((item)=>{
-        return (
-          <div key={item.title}>
-          <Border classes="rounded-2xl group h-full">
-            <div className="h-full rounded-2xl bg-black p-6">
-              <div className="mx-auto mb-4 w-[75%] md:group-hover:hidden">
-                <Image src={item.image} alt={item.title} />
+            );
+          })}
+        </>
+      ) : (
+        <>
+          {items.map((item) => {
+            return (
+              <div key={item.title}>
+                <Border classes="rounded-2xl group h-full">
+                  <div className="h-full rounded-2xl bg-black p-6">
+                    <div className="mx-auto mb-4 w-[75%] md:group-hover:hidden">
+                      <Image src={item.image} alt={item.title} />
+                    </div>
+                    <p className="mb-2 text-center text-5xl font-bold text-white">
+                      {item.title}
+                    </p>
+                    <p className="body mt-s4 hidden cursor-pointer text-left group-hover:block">
+                      {item.description}
+                    </p>
+                  </div>
+                </Border>
               </div>
-              <p className="mb-2 text-center text-5xl font-bold text-white">
-                {item.title}
-              </p>
-              <p className="body hidden text-left group-hover:block mt-s4 cursor-pointer">
-                {item.description}
-              </p>
-            </div>
-          </Border>
-        </div>
-        )
-      })
-     }
-       
-     </>
-     }
-   </div>
+            );
+          })}
+        </>
+      )}
+    </div>
   );
 }
-
-
-//  ${!isHovered ? 'gap-y-6 py-s4' : 'gap-y-2'}
--- components/layout/StaggeredTextAndImage.jsx
+++ components/layout/StaggeredTextAndImage.jsx
@@ -2,57 +2,67 @@ import Image from 'next/image';
 
 export default function StaggeredTextAndGraphic({ items, staggeredStyle }) {
   return (
-    <div className={`grid ${staggeredStyle ?'gap-y-s2 my-s5' : 'gap-s5 md:gap-s8'}`}>
-     {
-      staggeredStyle ? 
-      <>
-       {items.map((item, i) => (
-        <div
-          className="flex flex-col md:flex-row gap-s2 md:gap-s3 md:items-center justify-between"
-          key={item.title}
-        >
-          <div className={`order-2 w-full md:w-[50%] md:pl-s10 ${i % 2 != 0 ? 'md:order-1' : ''}`}>
-            <h3 className="mb-s1 mtext-2xl font-bold text-white">
-              {item.title}
-            </h3>
-            <p className="w-[65%] body">{item.description}</p>
-          </div>
-          <div
-            className={`order-1 group relative mx-auto w-full h-full md:w-[45%] cursor-pointer ${
-              i % 2 != 0 ? 'order-2' : ''
-            }`}
-          >
-            <Image src={item.image} alt={item.title} />
-            <div className={"gradient-1 transition-300 absolute left-1/2 top-1/2 -z-10 h-[104%] w-[104%] -translate-x-1/2 -translate-y-1/2 rounded-2xl opacity-70 md:opacity-0 blur-2xl md:blur-3xl md:group-hover:opacity-90"}
-            ></div>
-          </div>
-        </div>
-      ))}
-      </>
-      :
-      <>
-       {items.map((item, i) => (
-        <div
-          className="grid items-center gap-s2.5 md:grid-cols-2"
-          key={item.title}
-        >
-          <div className={`order-2 ${i % 2 === 0 ? 'md:order-1' : ''}`}>
-            <h3 className="mb-s2 text-5xl font-bold text-white">
-              {item.title}
-            </h3>
-            <p className="body">{item.description}</p>
-          </div>
-          <div
-            className={`order-1 mx-auto w-[240px] md:w-[300px] ${
-              i % 2 === 0 ? 'order-2' : ''
-            }`}
-          >
-            <Image src={item.image} alt={item.title} />
-          </div>
-        </div>
-      ))}
-      </>
-     }
+    <div
+      className={`grid ${
+        staggeredStyle ? 'my-s5 gap-y-s2' : 'gap-s5 md:gap-s8'
+      }`}
+    >
+      {staggeredStyle ? (
+        <>
+          {items.map((item, i) => (
+            <div
+              className="flex flex-col justify-between gap-s2 md:flex-row md:items-center md:gap-s3"
+              key={item.title}
+            >
+              <div
+                className={`order-2 w-full md:w-[50%] md:pl-s10 ${
+                  i % 2 != 0 ? 'md:order-1' : ''
+                }`}
+              >
+                <h3 className="gradient-text gradient-1 mb-s1 text-2xl font-bold">
+                  {item.title}
+                </h3>
+                <p className="body w-[65%]">{item.description}</p>
+              </div>
+              <div
+                className={`group relative order-1 mx-auto h-full w-full cursor-pointer md:w-[45%] ${
+                  i % 2 != 0 ? 'order-2' : ''
+                }`}
+              >
+                <Image src={item.image} alt={item.title} />
+                <div
+                  className={
+                    'gradient-1 transition-300 absolute left-1/2 top-1/2 -z-10 h-[104%] w-[104%] -translate-x-1/2 -translate-y-1/2 rounded-2xl opacity-70 blur-2xl md:opacity-0 md:blur-3xl md:group-hover:opacity-90'
+                  }
+                ></div>
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <>
+          {items.map((item, i) => (
+            <div
+              className="grid items-center gap-s2.5 md:grid-cols-2"
+              key={item.title}
+            >
+              <div className={`order-2 ${i % 2 === 0 ? 'md:order-1' : ''}`}>
+                <h3 className="mb-s2 text-5xl font-bold text-white">
+                  {item.title}
+                </h3>
+                <p className="body">{item.description}</p>
+              </div>
+              <div
+                className={`order-1 mx-auto w-[240px] md:w-[300px] ${
+                  i % 2 === 0 ? 'order-2' : ''
+                }`}
+              >
+                <Image src={item.image} alt={item.title} />
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
