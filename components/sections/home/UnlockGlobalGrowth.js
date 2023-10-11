3960346c66eef1cf97bfe208dc4d0466f6353fc9
chandhu <chandhudev0@gmail.com>
2023-10-11 18:16:47 +0530

    feat: adds the rotation of text animation in landing page

--- components/sections/home/UnlockGlobalGrowth.js
+++ components/sections/home/UnlockGlobalGrowth.js
@@ -6,6 +6,18 @@ const UnlockGlobalGrowth = () => {
   return (
     <section className="section m-horizontal mt-12 lg:mt-28">
       <div className="flex flex-col items-start justify-start md:items-center md:justify-center">
+        <div className="mt-10 mb-20 flex box-content">
+          <h2 className="title text-white">You create we - </h2>
+          <div className="h-10 ml-s1 mt-s1 leading-4 overflow-hidden">
+             <span className="roll text-7xl md:text-8xl font-bold gradient-text gradient-2">
+              transcibe<br/>
+              translate<br/>
+              edit<br/>
+              dub<br/>
+              distribute<br/>
+            </span>
+          </div>
+        </div>
         <h1 className="title">Unlock Global Growth, One Click at a Time</h1>
         <p className="body mt-2 mb-8 max-w-full md:mt-0 md:text-center">
           Monetize, translate, and distribute your content to millions worldwide
@@ -17,7 +29,7 @@ const UnlockGlobalGrowth = () => {
           </Button>
         </div>
         <div className="relative mt-12 h-[85%] cursor-pointer">
-          <video className="rounded-2xl" autoPlay loop muted>
+          <video className="rounded-md md:rounded-3xl" autoPlay loop muted>
             <source src={videoAsset} type="video/mp4" />
           </video>
           <div
