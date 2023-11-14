a936f74921a5ad6fc3dc36ec0464dd6a85b2e023
victor <vicopem@gmail.com>
2023-11-14 13:47:39 -0500

    Feat: Privacy policy

---
+++ components/sections/privacy-policy/policy.js
@@ -0,0 +1,324 @@
+const PrivacyData = () => {
+  return (
+    <div className="m-horizontal mt-s3 text-base text-white md:text-lg">
+      <h1 className="my-s4 text-3xl font-bold md:text-6xl">
+        Aview International Privacy Policy
+      </h1>
+      <div className="my-s4">
+        <h2 className="text-xl font-semibold underline md:text-3xl">Purpose</h2>
+        <h4 className="my-s2">
+          The purpose of this privacy policy (this &#34;Privacy Policy&#34;) is to
+          inform users of our Site of the following:
+        </h4>
+        <ol className="ml-s1 list-decimal">
+          <li>The personal data we will collect;</li>
+          <li className="my-s2">Use of collected data;</li>
+          <li>Who has access to the data collected;</li>
+          <li className="my-s2">The rights of Site users; and</li>
+          <li>The Site&#39;s cookie policy.</li>
+        </ol>
+        <br />
+        <p>
+          This Privacy Policy applies in addition to the terms and conditions of
+          our Site.
+        </p>
+      </div>
+      <h2 className="text-xl font-semibold underline md:text-3xl">GDPR</h2>
+      <p className="mt-s1 mb-s3">
+        For users in the European Union, we adhere to the Regulation (EU)
+        2016/679 of the European Parliament and of the Council of 27 April 2016,
+        known as the General Data Protection Regulation (the &#34;GDPR&#34;). For users
+        in the United Kingdom, we adhere to the GDPR as enshrined in the Data
+        Protection Act 2018.
+      </p>
+      <h2 className="text-xl font-semibold underline md:text-3xl">Consent</h2>
+      <p className="mt-s1 mb-s3">
+        By using our Site users agree that they consent to The conditions set
+        out in this Privacy Policy.
+      </p>
+      <p>
+        When the legal basis for us processing your personal data is that you
+        have provided your consent to that processing, you may withdraw your
+        consent at any time. If you withdraw your consent, it will not make
+        processing which we completed before you withdrew your consent unlawful.
+      </p>
+      <br />
+      <p>
+        You can withdraw your consent by: Contacting our Customer Experience
+        Officer, julia@aviewint.com.
+      </p>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Legal Basis for Processing
+      </h2>
+      <p className="mt-s1 mb-s3">
+        We collect and process personal data about users in the EU only when we
+        have a legal basis for doing so under Article 6 of the GDPR.
+      </p>
+      <p>
+        We rely on the following legal bases to collect and process the personal
+        data of users in the EU:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li>
+          Users have provided their consent to the processing of their data for
+          one or more specific purposes;
+        </li>
+        <li className="my-s2">
+          Users have provided their consent to the processing of their data for
+          one or more specific purposes;
+        </li>
+        <li>
+          Processing of user personal data is necessary to a task carried out in
+          the public interest or in the exercise of our official authority.
+        </li>
+      </ol>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Personal Data We Collect
+      </h2>
+      <p className="mt-s1 mb-s3">
+        We only collect data that helps us achieve the purpose set out in this
+        Privacy Policy. We will not collect any additional data beyond the data
+        listed below without notifying you first.
+      </p>
+      <h3 className="underline">Data Collected in a Non-Automatic Way</h3>
+      <p className="mt-s1 mb-s3">
+        We may also collect the following data when you perform certain
+        functions on our Site:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li>First and last name;</li>
+        <li className="my-s2">Email address;</li>
+        <li>Payment information;</li>
+        <li className="my-s2">Auto fill data; and</li>
+        <li>Social media uploads and analytics.</li>
+      </ol>
+      <p className="mt-s2">
+        This data may be collected using the following methods:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li className="my-s2">Creating an account; and</li>
+        <li>
+          Linking social media account (Meta, TikTok, YouTube, Snapchat,
+          LinkedIn.
+        </li>
+      </ol>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        How We Use Personal Data
+      </h2>
+      <p className="mt-s1 mb-s3">
+        Data collected on our Site will only be used for the purposes specified
+        in this Privacy Policy or indicated on the relevant pages of our Site.
+        We will not use your data beyond what we disclose in this Privacy
+        Policy.
+      </p>
+      <p>
+        The data we collect when the user performs certain functions may be used
+        for the following purposes:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li>Determine the best content to translate, dub and distribute.</li>
+      </ol>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Who We Share Personal Data With
+      </h2>
+      <h3 className="underline">Employees</h3>
+      <p className="mt-s1 mb-s3">
+        We may disclose user data to any member of our organization who
+        reasonably needs access to user data to achieve the purposes set out in
+        this Privacy Policy.
+      </p>
+      <h3 className="underline">Other Disclosures</h3>
+      <p className="mt-s1 mb-s3">
+        We will not sell or share your data with other third parties, except in
+        the following cases:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li>If the law requires it;</li>
+        <li className="my-s2">If it is required for any legal proceeding;</li>
+        <li>To prove or protect our legal rights; and</li>
+        <li className="my-s2">
+          To buyers or potential buyers of this company in the event that we
+          seek to sell the company.
+        </li>
+      </ol>
+      <p>
+        If you follow hyperlinks from our Site to another Site, please note that
+        we are not responsible for and have no control over their privacy
+        policies and practices.
+      </p>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        How Long We Store Personal Data
+      </h2>
+      <p className="mt-s1 mb-s3">
+        User data will be stored until the purpose the data was collected for
+        has been achieved.
+      </p>
+      <p>
+        You will be notified if your data is kept for longer than this period.
+      </p>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        How We Protect Your Personal Data
+      </h2>
+      <p className="mt-s1 mb-s3">
+        In order to protect your security, we use the strongest available
+        browser encryption and store all of our data on servers in secure
+        facilities. All data is only accessible to our employees. Our employees
+        are bound by strict confidentiality agreements and a breach of this
+        agreement would result in the employee&#39;s termination.
+      </p>
+      <p>
+        While we take all reasonable precautions to ensure that user data is
+        secure and that users are protected, there always remains the risk of
+        harm. The Internet as a whole can be insecure at times and therefore we
+        are unable to guarantee the security of user data beyond what is
+        reasonably practical.
+      </p>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Your Rights as a User
+      </h2>
+      <p className="mt-s1 mb-s3">
+        Under the GDPR, you have the following rights:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li>Right to be informed;</li>
+        <li className="my-s2">Right of access;</li>
+        <li>Right to rectification;</li>
+        <li className="my-s2">Right to erasure;</li>
+        <li>Right to restrict processing;</li>
+        <li className="my-s2">Right to data portability; and</li>
+        <li>Right to object.</li>
+      </ol>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">Children</h2>
+      <p className="mt-s1 mb-s3">
+        We do not knowingly collect or use personal data from children under 16
+        years of age. If we learn that we have collected personal data from a
+        child under 16 years of age, the personal data will be deleted as soon
+        as possible. If a child under 16 years of age has provided us with
+        personal data their parent or guardian may contact our data protection
+        officer.
+      </p>
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        How to Access, Modify, Delete, or Challenge the Data Collected
+      </h2>
+      <p className="mt-s1 mb-s3">
+        If you would like to know if we have collected your personal data, how
+        we have used your personal data, if we have disclosed your personal data
+        and to who we disclosed your personal data, if you would like your data
+        to be deleted or modified in any way, or if you would like to exercise
+        any of your other rights under the GDPR, please contact our data
+        protection officer here:
+      </p>
+      <p>
+        Julia McKay <br />
+        julia@aviewint.com <br />
+        (905) 902-1724
+      </p>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Do Not Track Notice
+      </h2>
+      <p className="mt-s1 mb-s3">
+        Do Not Track (&#34;DNT&#34;) is a privacy preference that you can set in certain
+        web browsers. We do not track the users of our Site over time and across
+        third party websites and therefore do not respond to browser- initiated
+        DNT signals.
+      </p>
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        How to Opt-Out of Data Collection, Use or Disclosure
+      </h2>
+      <p className="mt-s1 mb-s3">
+        In addition to the method(s) described in the How to Access, Modify,
+        Delete, or Challenge the Data Collected section, we provide the
+        following specific opt-out methods for the forms of collection, use, or
+        disclosure of your personal data specified below:
+      </p>
+      <ol className="ml-s1 list-decimal">
+        <li>
+          You can opt-out of the use of your personal data for marketing emails.
+          You can opt-out by clicking &#34;unsubscribe&#34; on the bottom of any
+          marketing email or updating your email preferences under your account.
+        </li>
+      </ol>
+      <br />
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Cookie Policy
+      </h2>
+      <p className="mt-s1 mb-s3">
+        A cookie is a small file, stored on a user&#39;s hard drive by a website.
+        Its purpose is to collect data relating to the user&#39;s browsing habits.
+        You can choose to be notified each time a cookie is transmitted. You can
+        also choose to disable cookies entirely in your internet browser, but
+        this may decrease the quality of your user experience.
+      </p>
+      <p>We use the following types of cookies on our Site:</p>
+      <br />
+      <ol className="ml-s1 list-decimal">
+        <li>
+          <p className="underline">Functional cookies</p>
+          Functional cookies are used to remember the selections you make on our
+          Site so that your selections are saved for your next visits;
+        </li>
+        <li className="my-s2">
+          <p className="underline">Analytical cookies</p>
+          Analytical cookies allow us to improve the design and functionality of
+          our Site by collecting data on how you access our Site, for example
+          data on the content you access, how long you stay on our Site, etc;
+        </li>
+        <li>
+          <p className="underline">Targeting cookies</p>
+          Targeting cookies collect data on how you use the Site and your
+          preferences. This allows us to personalize the information you see on
+          our Site for you; and
+        </li>
+        <li className="my-s2">
+          <p className="underline">Third-Party Cookies</p>
+          Third-party cookies are created by a website other than ours. We may
+          use third-party cookies to Monitor user preferences to tailor
+          advertisements around their interests
+        </li>
+      </ol>
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Modifications
+      </h2>
+      <p className="mt-s1 mb-s3">
+        This Privacy Policy may be amended from time to time in order to
+        maintain compliance with the law and to reflect any changes to our data
+        collection process. When we amend this Privacy Policy we will update the
+        &#34;Effective Date&#34; at the top of this Privacy Policy. We recommend that
+        our users periodically review our Privacy Policy to ensure that they are
+        notified of any updates. If necessary, we may notify users by email of
+        changes to this Privacy Policy.
+      </p>
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Complaints
+      </h2>
+      <p className="mt-s1 mb-s3">
+        If you have any complaints about how we process your personal data,
+        please contact us through the contact methods listed in the Contact
+        Information section so that we can, where possible, resolve the issue.
+        If you feel we have not addressed your concern in a satisfactory manner
+        you may contact a supervisory authority. You also have the right to
+        directly make a complaint to a supervisory authority. You can lodge a
+        complaint with a supervisory authority by contacting the Office of the
+        Privacy Commissioner of Canada.
+      </p>
+      <h2 className="text-xl font-semibold underline md:text-3xl">
+        Contact Information
+      </h2>
+      <p className="mt-s1 mb-s3">
+        If you have any questions, concerns or complaints, you can contact our
+        data protection officer, Julia McKay, at: julia@aviewint.com
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyData;
