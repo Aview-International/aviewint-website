0583b7f2fd9e5b7990c845b26643a3b0f63d855d
victor <vicopem@gmail.com>
2023-11-30 12:12:46 -0500

    Fix: Redirect fix

--- pages/social/instagram.js
+++ pages/social/instagram.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import {
+  completeIgConnection,
   getInstagramLongLivedAccess,
   getInstagramProfile,
   getInstagramShortAccess,
