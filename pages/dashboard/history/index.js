314a3bf378caf78404538eff7df780d57ae7900d
victor <vicopem@gmail.com>
2024-02-06 14:04:45 -0500

    Feat: Onboarding animation and history redesign

--- pages/dashboard/history/index.js
+++ pages/dashboard/history/index.js
@@ -2,11 +2,10 @@ import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
 import DashboardLayout from '../../../components/dashboard/DashboardLayout';
 import PageTitle from '../../../components/SEO/PageTitle';
-import { getAllCompletedJobs, getAllPendingJobs } from '../../api/firebase';
+import { getAllPendingJobs } from '../../api/firebase';
 
 const History = () => {
   const [pendingJobs, setPendingJobs] = useState([]);
-  const [completedJobs, setCompletedJobs] = useState([]);
   const [reloadTrigger, setReloadTrigger] = useState(0);
   const uid = Cookies.get('uid');
 
@@ -21,97 +20,65 @@ const History = () => {
         : []
     );
   };
-  const getCompletedJobs = async () => {
-    const res = await getAllCompletedJobs(uid);
-    setCompletedJobs(
-      res
-        ? Object.values(res).map((item, i) => ({
-            ...item,
-            jobId: Object.keys(res)[i],
-          }))
-        : []
-    );
-  };
 
-  const getAll = async () =>
-    await Promise.all([getPendingJobs(), getCompletedJobs()]);
+  const getAll = async () => await Promise.all([getPendingJobs()]);
 
   useEffect(() => {
     if (uid) getAll();
   }, [reloadTrigger]);
 
+  console.log(pendingJobs);
+
   return (
     <>
       <PageTitle title="History" />
       <h2 className="mt-s2 text-4xl">History</h2>
-      <Container pendingJobs={pendingJobs} completedJobs={completedJobs} />
+      <Container pendingJobs={pendingJobs} />
     </>
   );
 };
 
-const Container = ({ pendingJobs, completedJobs }) => {
+const Container = ({ pendingJobs }) => {
   return (
     <div className="w-full rounded-2xl bg-gradient-to-b from-[#ffffff26] to-[#ffffff0D] p-s3">
-      <div className="grid grid-cols-[30%_20%_30%_20%]">
+      <div className="grid grid-cols-[25%_20%_22%_20%_13%]">
         <p>Name</p>
         <p>Date</p>
         <p>Languages</p>
         <p>Status</p>
+        <p>Download Link</p>
       </div>
       <hr className="my-s2 border-[rgba(255,255,255,0.6)]" />
       {pendingJobs.map((job, i) => (
         <div
-          className="grid grid-cols-[30%_20%_30%_20%] border-b border-[rgba(252,252,252,0.2)] py-s2"
+          className="grid grid-cols-[25%_20%_25%_20%_10%] border-b border-[rgba(252,252,252,0.2)] py-s2"
           key={i}
         >
+          <div>{job.videoData.caption}</div>
+          <p>{new Date(+job.timestamp).toDateString()}</p>
           <div>
-            {job.videoData.map(({ caption }, idx) => (
-              <p key={idx} className="mb-s1">
-                {caption}
-              </p>
-            ))}
-          </div>
-          <p>{new Date(job.createdAt).toDateString()}</p>
-          <div>
-            {job.languages.map((lang, idx) => (
+            {job?.languages.map((lang, idx) => (
               <p key={idx} className="mb-s1">
                 {lang}
               </p>
             ))}
           </div>
-          <div className="text-[#eab221]">In progress</div>
-        </div>
-      ))}
-
-      {completedJobs.map((job, i) => (
-        <div
-          className="grid grid-cols-[30%_20%_20%_30%] border-b border-[rgba(252,252,252,0.2)] py-s2"
-          key={i}
-        >
+          <div className="text-[#eab221]">{job.status}</div>
           <div>
-            {job.videoData.map(({ title }, idx) => (
-              <p key={idx} className="mb-s1">
-                {title}
-              </p>
-            ))}
-          </div>
-          <p>{new Date(job.createdAt).toDateString()}</p>
-          <div>
-            {job.services.map((service, idx) => (
-              <p key={idx} className="mb-s1">
-                {service}
-              </p>
-            ))}
-          </div>
-          <div>
-            <a
-              href={job.jobUrl}
-              target="_blank"
-              rel="noreferrer"
-              className="mb-s1 block text-blue underline"
-            >
-              {job.jobUrl}
-            </a>
+            {job?.downloadLink &&
+              Object.keys(job.downloadLink).map((el, idx) => (
+                <span key={idx}>
+                  {el}:{' '}
+                  <a
+                    href={job.downloadLink[el]}
+                    target="_blank"
+                    className="text-blue underline"
+                    rel="noreferrer"
+                  >
+                    Download
+                  </a>
+                </span>
+              ))}
           </div>
         </div>
       ))}
