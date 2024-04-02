f88a50c6e336e5e7140288a5c4ba4441d41d3707
victor <vicopem@gmail.com>
2024-04-02 18:57:15 -0400

    Fix: Auth issues

--- services/baseUrl.js
+++ services/baseUrl.js
@@ -2,5 +2,3 @@ export const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'https://pre-prod.aviewint.com/'
     : 'https://api.aviewint.com/';
-
-// export const baseUrl = 'http://localhost:4000';
