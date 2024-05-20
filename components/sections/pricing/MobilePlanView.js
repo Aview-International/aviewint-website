02c7cf6f7b94a7f2d889a1579f50652e5913c84c
chandhu <chandhudev0@gmail.com>
2024-05-20 13:45:58 +0530

    update: updates the mobile pricing header

--- components/sections/pricing/MobilePlanView.js
+++ components/sections/pricing/MobilePlanView.js
@@ -2,19 +2,13 @@ import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import Check from '../../../public/img/icons/check.svg';
 import Info from '../../../public/img/icons/info.svg';
-//Needs these from pricing plan:?
-import Border from '../../UI/Border';
-import Card from '../../UI/Card';
-import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
-
 
 const MobilePlanView = ({ plans }) => {
   const [visibleHoverText, setVisibleHoverText] = useState(null);
   const headerRef = useRef(null);
   const [topPosition, setTopPosition] = useState(null);
 
-  const planTypes = ["Starter Studio", "Creator Pro", "Global Influencer"];
+  const planTypes = ['Starter Studio', 'Creator Pro', 'Global Influencer'];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,7 +22,6 @@ const MobilePlanView = ({ plans }) => {
     };
   }, []);
 
-
   const handleMouseEnter = (index) => {
     setVisibleHoverText(index);
   };
@@ -38,39 +31,52 @@ const MobilePlanView = ({ plans }) => {
   };
 
   return (
-    <div className="block md:hidden bg-gray-800 text-white p-8">
-      <div className={`sticky top-0 z-10 text-center mb-10 grid grid-cols-3 rounded-lg p-s3
-                        gap-3 border-b border-gray-200 
-          ${
-            topPosition <= 0 ? 'gradient-dark bg-black' : ''
-          }`}
-          ref={headerRef}
-          >
-
+    <div className="bg-gray-800 text-white md:hidden">
+      <div
+        className={`border-gray-200 sticky top-0 z-10  grid grid-cols-3 gap-3 rounded-lg  py-s3 px-s1 text-center 
+        ${topPosition <= 0 ? 'gradient-dark rounded-b-lg bg-black' : ''}`}
+        ref={headerRef}
+      >
         {planTypes.map((header, index) => (
-          <div key={index} className={`text-md ${index === 1 ? 'text-gray-200' : 'text-white'}`}>
-            {header}
+          <div
+            key={index}
+            className={`text-md ${
+              index === 1 ? 'text-gray-200' : 'text-white'
+            }`}
+          >
+            <p className="text-sm text-white/95">{header}</p>
           </div>
         ))}
       </div>
-
+      <div className="bg-white/65 mx-4 mb-10 border-x-0 border-y border-t-0"></div>
       {plans.map((section, sectionIndex) => (
-        <div key={sectionIndex} className="mb-14">
-          <div className="font-bold text-xl mb-8">{section.section}</div>
+        <div key={sectionIndex} className="mb-14 p-4">
+          <div className="mb-8 text-xl font-bold">{section.section}</div>
           {section.desc.map((feature, featureIndex) => (
             <div key={featureIndex} className="mb-8">
-              <div className="flex justify-between items-center border-b border-gray-100 mb-8 pb-5">
+              <div className="border-gray-100 mb-8 flex items-center justify-between border-b pb-5">
                 <span className="text-sm">{feature.title}</span>
 
                 {feature.hoverText && (
-                  <div 
-                    onMouseEnter={() => handleMouseEnter(sectionIndex + '-' + featureIndex)}
+                  <div
+                    onMouseEnter={() =>
+                      handleMouseEnter(sectionIndex + '-' + featureIndex)
+                    }
                     onMouseLeave={handleMouseLeave}
-                    onClick={() => handleMouseEnter(sectionIndex + '-' + featureIndex)}
-                    className="relative cursor-pointer">
-                    <Image src={Info} alt="" width={20} height={20} layout="fixed" />
+                    onClick={() =>
+                      handleMouseEnter(sectionIndex + '-' + featureIndex)
+                    }
+                    className="relative cursor-pointer"
+                  >
+                    <Image
+                      src={Info}
+                      alt="check-info"
+                      width={20}
+                      height={20}
+                      layout="fixed"
+                    />
                     {visibleHoverText === sectionIndex + '-' + featureIndex && (
-                      <div className="absolute right-full mr-2 top-0 z-10 w-48 rounded-md bg-gray-1 p-s1 text-xs group-hover:inline text-white">
+                      <div className="absolute right-full top-0 z-10 mr-2 w-48 rounded-md bg-gray-1 p-s1 text-xs text-white group-hover:inline">
                         {feature.hoverText}
                       </div>
                     )}
@@ -78,21 +84,29 @@ const MobilePlanView = ({ plans }) => {
                 )}
               </div>
 
-              <div className="grid grid-cols-3 gap-2 text-sm border-b border-gray-100 pb-8">
+              <div className="border-gray-100 grid grid-cols-3 gap-2 border-b pb-8 text-sm">
                 {feature.columns.map((col, colIndex) => (
-
-                  <div key={colIndex} className={`p-2 text-center ${colIndex === 1 ? 'bg-gray-700' : 'bg-gray-800'}`}>
+                  <div
+                    key={colIndex}
+                    className={`p-2 text-center ${
+                      colIndex === 1 ? 'bg-gray-700' : 'bg-gray-800'
+                    }`}
+                  >
                     {col === true ? (
-                      <Image src={Check} alt="" width={25} height={20} />
+                      <Image
+                        src={Check}
+                        alt="check-info"
+                        width={25}
+                        height={20}
+                      />
                     ) : col === null ? (
-                      <span className="mx-auto block h-1 w-4 bg-white"></span>
+                      <span className="mx-auto block h-1 w-4 bg-white/60"></span>
                     ) : (
                       <p className="text-sm md:text-base">{col}</p>
                     )}
                   </div>
                 ))}
               </div>
-
             </div>
           ))}
         </div>
--- components/sections/pricing/PlanBreakdown.js
+++ components/sections/pricing/PlanBreakdown.js
@@ -27,113 +27,103 @@ const PlanBreakdown = ({ isChecked, handleChange, allPlans }) => {
   return (
     <section className="m-horizontal my-s10 overflow-x-clip" id="all-features">
       <div className="rounded-xl bg-white-transparent text-white">
-        <h2 className="py-s4 text-center text-3xl md:text-5xl font-semibold">
+        <h2 className="py-s4 text-center text-5xl font-semibold">
           Compare Plans
         </h2>
-
-        {/* Desktop and tablet view here */}
         <div className="hidden md:block">
-
-        <div
-          className={`sticky top-0 z-10 grid grid-cols-1 md:grid-cols-[repeat(4,minmax(100px,1fr))] rounded-xl p-s4 text-center 
-          ${
-            topPosition <= 0 ? 'gradient-dark bg-black' : ''
-          }`}
-          ref={headerRef}
-        >
-          <div className="mb-4 md:mb-0">
-            <ToggleButton isChecked={isChecked} handleChange={handleChange} />
-            <p className="text-sm md:text-base">
-              Annual<br />(save up to 28%)
-            </p>
-          </div>
-
-          {allPlans.map((plan, i) => (
-            <div key={i} className="mb-4 md:mb-0">
-              <div className="text-xl md:text-2xl">{plan.desc}</div>
-              <p className="my-2 text-lg md:text-xl font-medium">
-                {plan.id === 'enterprise'
-                  ? 'Contact Sales'
-                  : `${
-                      (typeof plan.monthlyCost && typeof plan.yearlyCost) === 'number'
-                        ? '$'
-                        : ''
-                    }${isChecked
-                        ? Math.round(plan.yearlyCost / 12) || 'Free'
-                        : plan.monthlyCost}`
-                }
+          <div
+            className={`sticky top-0 z-10 grid grid-cols-1 rounded-xl p-s4 text-center md:grid-cols-[repeat(4,minmax(100px,1fr))] 
+          ${topPosition <= 0 ? 'gradient-dark bg-black' : ''}`}
+            ref={headerRef}
+          >
+            <div className="mb-4 md:mb-0">
+              <ToggleButton isChecked={isChecked} handleChange={handleChange} />
+              <p className="text-sm md:text-base">
+                Annual
+                <br />
+                (save up to 28%)
               </p>
-              <Button
-                type="secondary"
-                purpose="route"
-                route={
-                  plan.id === 'enterprise'
-                    ? 'mailto:julia@aviewint.com'
-                    : `/register?subscription=true&plan=${plan.id}`
-                }
-              >
-                Go {plan.id}
-              </Button>
             </div>
-          ))}
-        </div>
 
-        
-
-        {PLANS_BREAKDOWN.map((plan, i) => (
-          <div className="px-s4 py-s3" key={i}>
-            <div className="border-b border-gray-1 px-5 pb-5">
-              <p className="text-left text-sm md:text-base">{plan.section}</p>
-            </div>
-            {plan.desc.map((breakdown, i) => (
-              <div
-                key={`breakdown-${i}`}
-                className="grid grid-cols-1 md:grid-cols-4 items-center justify-center border-b border-gray-1 text-center"
-              >
-                <div className="flex items-center justify-between p-5 text-left text-base md:text-lg">
-                  <span className="pr-4">{breakdown.title}</span>
-                  {breakdown.hoverText && (
-                    <div className="group relative cursor-pointer">
-                      <Image
-                        src={Info}
-                        alt=""
-                        width={20}
-                        height={20}
-                        layout="fixed"
-                      />
-                      <span className="absolute z-10 hidden w-48 rounded-md bg-gray-1 p-s1 text-xs group-hover:inline">
-                        {breakdown.hoverText}
-                      </span>
-                    </div>
-                  )}
-                </div>
-                {breakdown.columns.map((col, i) => (
-                  <div className="p-5" key={i}>
-                    {col === true ? (
-                      <Image src={Check} alt="" width={25} height={20} />
-                    ) : col === null ? (
-                      <span className="mx-auto block h-1 w-4 bg-white"></span>
-                    ) : (
-                      <p className="text-sm md:text-base">{col}</p>
-                    )}
-                  </div>
-                ))}
+            {allPlans.map((plan, i) => (
+              <div key={i} className="mb-4 md:mb-0">
+                <div className="text-xl md:text-2xl">{plan.desc}</div>
+                <p className="my-2 text-lg font-medium md:text-xl">
+                  {plan.id === 'enterprise'
+                    ? 'Contact Sales'
+                    : `${
+                        (typeof plan.monthlyCost && typeof plan.yearlyCost) ===
+                        'number'
+                          ? '$'
+                          : ''
+                      }${
+                        isChecked
+                          ? Math.round(plan.yearlyCost / 12) || 'Free'
+                          : plan.monthlyCost
+                      }`}
+                </p>
+                <Button
+                  type="secondary"
+                  purpose="route"
+                  route={
+                    plan.id === 'enterprise'
+                      ? 'mailto:julia@aviewint.com'
+                      : `/register?subscription=true&plan=${plan.id}`
+                  }
+                >
+                  Go {plan.id}
+                </Button>
               </div>
             ))}
           </div>
-        ))}
-      </div>
 
-
-       {/* Mobile view - shown only on screens <=  md */}
-       <div className="md:hidden">
+          {PLANS_BREAKDOWN.map((plan, i) => (
+            <div className="px-s4 py-s3" key={i}>
+              <div className="border-b border-gray-1 px-5 pb-5">
+                <p className="text-left text-sm md:text-base">{plan.section}</p>
+              </div>
+              {plan.desc.map((breakdown, i) => (
+                <div
+                  key={`breakdown-${i}`}
+                  className="grid grid-cols-1 items-center justify-center border-b border-gray-1 text-center md:grid-cols-4"
+                >
+                  <div className="flex items-center justify-between p-5 text-left text-base md:text-lg">
+                    <span className="pr-4">{breakdown.title}</span>
+                    {breakdown.hoverText && (
+                      <div className="group relative cursor-pointer">
+                        <Image
+                          src={Info}
+                          alt=""
+                          width={20}
+                          height={20}
+                          layout="fixed"
+                        />
+                        <span className="absolute z-10 hidden w-48 rounded-md bg-gray-1 p-s1 text-xs group-hover:inline">
+                          {breakdown.hoverText}
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                  {breakdown.columns.map((col, i) => (
+                    <div className="p-5" key={i}>
+                      {col === true ? (
+                        <Image src={Check} alt="" width={25} height={20} />
+                      ) : col === null ? (
+                        <span className="mx-auto block h-1 w-4 bg-white"></span>
+                      ) : (
+                        <p className="text-sm md:text-base">{col}</p>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="md:hidden">
           <MobilePlanView plans={PLANS_BREAKDOWN} />
         </div>
-
       </div>
-
-     
-    
     </section>
   );
 };
