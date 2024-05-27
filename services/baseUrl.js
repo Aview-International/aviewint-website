c9ef5133c3d90d297e5ae5dd7e18014fde880954
victor <vicopem@gmail.com>
2024-05-27 13:24:35 -0400

    Fix: IG test account

--- services/baseUrl.js
+++ services/baseUrl.js
@@ -1,6 +1,6 @@
-export const baseUrl =
-process.env.NODE_ENV === 'development'
-? 'https://pre-prod.aviewint.com/'
-: 'https://api.aviewint.com/';
+// export const baseUrl =
+// process.env.NODE_ENV === 'development'
+// ? 'https://pre-prod.aviewint.com/'
+// : 'https://api.aviewint.com/';
 
-// export const baseUrl = 'http://localhost:4000/';
+export const baseUrl = 'http://localhost:4000/';
