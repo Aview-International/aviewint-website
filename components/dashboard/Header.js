7123cca65f6ca3a2a06b4a1f22fe736753de4c6e
chandhu <chandhudev0@gmail.com>
2024-06-06 17:03:56 +0530

    create: creates the usage limit of the user on dashboard header

--- components/dashboard/Header.js
+++ components/dashboard/Header.js
@@ -1,8 +1,5 @@
-import Image from 'next/image';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useMemo } from 'react';
 import MenuOpenContext from '../../store/menu-open-context';
-import Link from 'next/link';
-import messages from '../../public/img/icons/messages.svg';
 import DashboardMobileMenu from '../navigation/DashboardMobileMenu';
 import MenuButtonIcon from '../navigation/MenuButtonIcon';
 import { customGreeting } from '../../utils/greeting';
@@ -10,7 +7,35 @@ import { useSelector } from 'react-redux';
 
 const DashBoardHeader = ({ userInfo }) => {
   const [time, setTime] = useState(customGreeting());
-  const messageStatus = useSelector((state) => state.messages.status);
+  const { videoTimeLeft, plan } = useSelector((state) => state.user);
+
+  const getPlan = useMemo(() => {
+    let planLimit = 0;
+    if (plan === undefined) {
+      planLimit = 5;
+    } else if (plan === 'pro') {
+      planLimit = 45;
+    } else if (plan === 'unlimited') {
+      planLimit = 300;
+    }
+    return planLimit;
+  }, [plan]);
+
+  const circumference = 157;
+  const videoTimeLeftInMinutes = useMemo(() => {
+    return videoTimeLeft / 60;
+  }, [videoTimeLeft]);
+
+  const percentage = useMemo(() => {
+    const val = (videoTimeLeftInMinutes / getPlan) * 100;
+    return Math.round(val);
+  }, [videoTimeLeftInMinutes, getPlan]);
+
+  const offset = useMemo(() => {
+    const val = (percentage / 100) * circumference;
+    return Math.round(val);
+  }, [percentage, circumference]);
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTime(customGreeting());
@@ -22,9 +47,8 @@ const DashBoardHeader = ({ userInfo }) => {
   }, []);
 
   const menuOpenCtx = useContext(MenuOpenContext);
-
   return (
-    <header className="relative flex w-full items-center justify-between px-s4 py-s3 text-white md:px-s9">
+    <header className="relative flex w-full items-center justify-between px-s4 pt-s2 pb-s1.5 text-white md:px-s9">
       <MenuButtonIcon
         handler={menuOpenCtx.openMenuHandler}
         styles={'absolute left-6'}
@@ -36,20 +60,58 @@ const DashBoardHeader = ({ userInfo }) => {
         </h3>
         <p className="text-lg text-gray-2">Welcome to your Aview Dashboard</p>
       </div>
-      <div className="hidden md:block">
-        <Link href="/dashboard/messages">
-          <a className="relative flex items-center rounded-full bg-gray-1 px-s2 py-2.5 text-sm">
-            <span className="mr-s1.5 grid place-content-center brightness-0 invert">
-              <Image src={messages} alt="Messages" />
-            </span>
-            <span className="mt-0.5">Messages</span>
-            {messageStatus.readByUser === false && (
-              <span className="absolute -bottom-0.5 -right-0.5 inline-block h-4 w-4 rounded-full bg-red" />
-            )}
-          </a>
-        </Link>
+      <div className="flex gap-x-4 bg-gray-1 rounded-xl py-1">
+        <div className="hidden md:block">
+          <Circle videoTimeLeft={videoTimeLeftInMinutes} offset={offset} />
+          <p className="mt-2 text-center text-xs">Translations</p>
+        </div>
+        <div className="hidden md:block">
+          <Circle videoTimeLeft={videoTimeLeftInMinutes} offset={offset} />
+          <p className="mt-2 text-center text-xs">Dubbing</p>
+        </div>
+        <div className="hidden md:block">
+          <Circle videoTimeLeft={videoTimeLeftInMinutes} offset={offset} />
+          <p className="mt-2 text-center text-xs">Transcriptions</p>
+        </div>
+        {/* <div className="hidden md:block">
+          <Circle videoTimeLeft={videoTimeLeft} offset={offset} />
+          <p className="mt-2 text-center text-xs">AI Voiceovers</p>
+        </div> */}
       </div>
     </header>
+  );
+};
+
+const Circle = ({ offset, videoTimeLeft }) => {
+ console.log(offset)
+  return (
+    <div className="relative mx-5 flex h-14 w-14 items-center justify-center">
+      <p className="mt-2 text-sm font-semibold text-center">
+        {Math.round(videoTimeLeft)}
+        <span className="block text-center text-[10px]" style={{ lineHeight: '0.75rem'}}>min</span>
+      </p>
+      <svg className="absolute -rotate-90" width="56" height="56">
+        <circle
+          cx="28"
+          cy="28"
+          r="25"
+          fill="none"
+          stroke="#000017"
+          strokeWidth="7"
+        ></circle>
+        <circle
+          cx="28"
+          cy="28"
+          r="25"
+          fill="none"
+          stroke="#00ffff"
+          strokeWidth="4"
+          strokeDasharray={157}
+          strokeDashoffset={offset}
+          style={{ transition: 'stroke-dashoffset 0.5s ease-in-out' }}
+        ></circle>
+      </svg>
+    </div>
   );
 };
 
