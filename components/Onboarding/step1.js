f36286cb4ec8d8e083f4fba7bd3debc61ebb62b4
chandhu <chandhudev0@gmail.com>
2024-06-11 15:40:15 +0530

    update: update the onboarding flow

--- components/Onboarding/step1.js
+++ components/Onboarding/step1.js
@@ -1,128 +1,158 @@
-import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import ComingSoon from '../UI/ComingSoon';
-import Border from '../UI/Border';
-import Image from 'next/image';
-import Shadow from '../UI/Shadow';
+import CustomSelectInput from '../FormComponents/CustomSelectInput';
+import MultipleSelectInput from '../FormComponents/MultipleSelectInput';
 import OnboardingButton from './button';
-import { ONBOARDING_STAGE_1 } from '../../constants/constants';
+import {
+  AVERAGE_MONTHLY_VIEWS,
+  AVERAGE_VIDEO_DURATION,
+  CATEGORIES,
+} from '../../constants/constants';
 import ErrorHandler from '../../utils/errorHandler';
 import {
   authCustomUser,
   updateRequiredServices,
 } from '../../services/firebase';
 
-const OnboardingStep1 = ({ userData }) => {
+export const OnboardingStep1 = ({ userData, allLanguages }) => {
   const router = useRouter();
   const [payload, setPayload] = useState({
-    role: '',
+    monthlyView: '',
+    categories: [],
+    averageVideoDuration: '',
+    defaultLanguage: '',
+  });
+  const [sideEffects, setSideEffects] = useState({
     hasSubmitted: false,
     isLoading: false,
   });
 
   useEffect(() => {
-    setPayload({ ...payload, role: userData.role });
+    setPayload({
+      monthlyView: userData.monthlyView,
+      categories: userData.categories,
+      averageVideoDuration: userData.averageVideoDuration,
+      defaultLanguage: userData.defaultLanguage,
+    });
   }, [userData]);
 
+  useEffect(() => {
+    if (
+      payload.monthlyView == null &&
+      payload.categories == null &&
+      payload.averageVideoDuration == null &&
+      payload.defaultLanguage == null
+    )
+      return;
+    const handleWarnUser = (event) => {
+      event.preventDefault();
+      return (event.returnValue = '');
+    };
+    window.addEventListener('beforeunload', handleWarnUser, { capture: true });
+    return () => {
+      window.removeEventListener('beforeunload', handleWarnUser, {
+        capture: true,
+      });
+    };
+  }, [payload]);
+
+  const isFormValid = () =>
+    !!payload.monthlyView &&
+    !!payload.categories.length >= 1 &&
+    !!payload.defaultLanguage &&
+    !!payload.averageVideoDuration;
+
   const handleSubmit = async () => {
-    setPayload({ ...payload, hasSubmitted: true });
-    if (!payload.role) return;
-    setPayload({ ...payload, isLoading: true });
+    setSideEffects({ ...sideEffects, hasSubmitted: true });
+    if (!isFormValid()) return;
+    setSideEffects({ ...sideEffects, isLoading: true });
     try {
-      const testUser = Cookies.get('testUser');
-      if (testUser) {
+      if (Cookies.get('testUser')) {
         await authCustomUser(
           Cookies.get('session'),
-          { role: payload.role },
+          payload,
           Cookies.get('uid')
         );
         return router.push('/onboarding?stage=2');
       }
-      await updateRequiredServices({ role: payload.role }, Cookies.get('uid'));
+      await updateRequiredServices(payload, Cookies.get('uid'));
       router.push('/onboarding?stage=2');
     } catch (error) {
-      setPayload({ ...payload, isLoading: false });
       ErrorHandler(error);
     }
   };
 
+  const handleMultipleLanguages = (option) => {
+    const newArray = [...payload.categories];
+    if (newArray.includes(option)) {
+      newArray.splice(newArray.indexOf(option), 1);
+      setPayload({ ...payload, categories: newArray });
+    } else {
+      newArray.push(option);
+      setPayload({ ...payload, categories: newArray });
+    }
+  };
+
   return (
-    <div className="m-auto w-[min(1000px,90%)]">
-      <h2 className="text-3xl md:text-center md:text-4xl">
-        How are you planning to use Aview?
+    <div className="m-auto w-[90%]">
+      <h2 className="text-4xl font-bold md:text-center md:text-6xl">
+        Tell us about yourself
       </h2>
-      <p className="mt-4 mb-8 text-lg text-white/90 md:text-center md:text-[19px]">
-        We&#8217;ll streamline your setup experience accordingly.
+      <p className="mt-s2 mb-s4 text-lg md:text-center md:text-xl">
+        We&#8217;ll customize your Aview experience based on your choices.
       </p>
-      <div className="flex flex-col items-stretch justify-center gap-4 lg:flex-row lg:gap-0">
-        {ONBOARDING_STAGE_1.map((option, index) => {
-          return (
-            <Fragment key={index}>
-              {option.desc === 'Coming Soon' ? (
-                <ComingSoon stage={1}>
-                  <Shadow classes="lg:w-[300px] md:h-[390px] w-full mr-s4 cursor-pointer">
-                    <Border borderRadius="2xl" classes="h-full w-full">
-                      <div
-                        className={`h-full rounded-2xl bg-black pt-s6 text-center md:pt-s9`}
-                      >
-                        <Image
-                          src={option.image}
-                          alt={option.title}
-                          width={172}
-                          height={172}
-                        />
-                        <div className="mt-3 flex h-[70px] flex-col md:mt-7">
-                          <h3 className="text-xl font-bold md:text-2xl">
-                            {option.title}
-                          </h3>
-                        </div>
-                      </div>
-                    </Border>
-                  </Shadow>
-                </ComingSoon>
-              ) : (
-                <Shadow classes="lg:w-[300px] md:h-[390px] w-full mr-s4 cursor-pointer">
-                  <Border borderRadius="2xl" classes="h-full w-full">
-                    <div
-                      className={`transition-300 h-full rounded-2xl bg-black p-s2 text-center ${
-                        payload.role === option.data && 'gradient-1'
-                      }`}
-                      onClick={() =>
-                        setPayload({ ...payload, role: option.data })
-                      }
-                    >
-                      <Image
-                        src={option.image}
-                        alt={option.title}
-                        width={172}
-                        height={172}
-                      />
-                      <div className="mt-3 flex h-full flex-col items-center justify-around md:mt-7 md:h-[127px]">
-                        <h3 className="text-xl font-bold md:text-2xl">
-                          {option.title}
-                        </h3>
-                        <p className="mt-s2 text-center text-lg md:text-xl">
-                          {option.desc}
-                        </p>
-                      </div>
-                    </div>
-                  </Border>
-                </Shadow>
-              )}
-            </Fragment>
-          );
-        })}
-      </div>
-      <div className="m-auto mt-s4 w-[min(360px,90%)]">
-        <OnboardingButton
-          disabled={!payload.role}
-          onClick={handleSubmit}
-          isLoading={payload.isLoading}
-          theme="light"
-        >
-          Continue
-        </OnboardingButton>
+      <div className="flex flex-col items-center justify-center ">
+        <div className="mt-s5 max-w-[1144px]">
+          <CustomSelectInput
+            hideCheckmark
+            text="What language is your channel?"
+            options={allLanguages}
+            hasSubmitted={sideEffects.hasSubmitted}
+            onChange={(option) =>
+              setPayload({ ...payload, defaultLanguage: option })
+            }
+            value={payload.defaultLanguage}
+          />
+          <CustomSelectInput
+            hideCheckmark
+            text="What are your average monthly views?"
+            options={AVERAGE_MONTHLY_VIEWS}
+            hasSubmitted={sideEffects.hasSubmitted}
+            onChange={(option) =>
+              setPayload({ ...payload, monthlyView: option })
+            }
+            value={payload.monthlyView}
+          />
+          <MultipleSelectInput
+            hideCheckmark
+            text="Which category does your content belongs to?"
+            options={CATEGORIES}
+            answer={payload.categories}
+            hasSubmitted={sideEffects.hasSubmitted}
+            onChange={(event) => handleMultipleLanguages(event)}
+          />
+          <CustomSelectInput
+            hideCheckmark
+            text="How long is your average duration of videos?"
+            options={AVERAGE_VIDEO_DURATION}
+            hasSubmitted={sideEffects.hasSubmitted}
+            onChange={(option) =>
+              setPayload({ ...payload, averageVideoDuration: option })
+            }
+            value={payload.averageVideoDuration}
+          />
+        </div>
+        <div className="m-auto mt-s2.5 w-[min(360px,90%)]">
+          <OnboardingButton
+            disabled={!isFormValid()}
+            onClick={handleSubmit}
+            isLoading={sideEffects.isLoading}
+            theme="light"
+          >
+            Continue
+          </OnboardingButton>
+        </div>
       </div>
     </div>
   );
--- components/Onboarding/step5.js
+++ components/Onboarding/step5.js
@@ -1,161 +1,99 @@
 import { useRouter } from 'next/router';
-import OnboardingButton from './button';
+import check from '../../public/img/icons/check.svg';
+import Border from '../UI/Border';
 import Image from 'next/image';
-import Trash from '../../public/img/icons/trash.svg';
-import { SUPPORTED_REGIONS } from '../../constants/constants';
-import { useEffect, useState } from 'react';
-import MultipleSelectInput from '../FormComponents/MultipleSelectInput';
-import ErrorHandler from '../../utils/errorHandler';
-import { useSelector } from 'react-redux';
-import {
-  authCustomUser,
-  updateRequiredServices,
-} from '../../services/firebase';
-import Cookies from 'js-cookie';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import OnboardingButton from './button';
 
-const OnboardingStep5 = ({ userData, allLanguages }) => {
+const OnboardingStep5 = ({ userData, plans }) => {
   const router = useRouter();
-  const youtubeChannel = useSelector((el) => el.youtube);
-  const [languages, setLanguages] = useState([]);
-  const [isError, setIsError] = useState(false);
-  const [selectLanguages, setSelectLanguages] = useState(false);
-
-  useEffect(() => {
-    setLanguages(userData.languages);
-  }, [userData.languages]);
-
-  const handleSubmit = async () => {
-    const payForPlan = localStorage.getItem('payForPlan');
-    try {
-      if (languages.length < 2) {
-        setIsError(true);
-        return;
-      }
-      const testUser = Cookies.get('testUser');
-      if (testUser) {
-        await authCustomUser(
-          Cookies.get('session'),
-          { languages },
-          Cookies.get('uid')
-        );
-        Cookies.remove('testUser');
-        return router.push('/onboarding?stage=6');
-      }
-      await updateRequiredServices({ languages }, userData.uid);
-      router.push(
-        `/onboarding?stage=${
-          payForPlan ? `subscription&plan=${payForPlan}` : '6'
-        }`
-      );
-    } catch (error) {
-      ErrorHandler(error);
-    }
-  };
-
-  const findLocalDialect = (language) => {
-    let tempLanguages = [];
-    SUPPORTED_REGIONS.forEach(({ data }) => {
-      data.forEach((el) => tempLanguages.push(el));
-    });
-    return tempLanguages.find((el) => el.languageName === language);
-  };
-
-  const handleRemoveLanguage = (language) => {
-    let tempLanguages = [...languages];
-    tempLanguages.splice(tempLanguages.indexOf(language), 1);
-    setLanguages(tempLanguages);
-  };
-
-  const handleMultipleLanguages = (option) => {
-    const tempLanguages = [...languages];
-    if (tempLanguages.includes(option))
-      tempLanguages.splice(tempLanguages.indexOf(option), 1);
-    else tempLanguages.push(option);
-    setLanguages(tempLanguages);
-  };
 
   return (
-    <div className="m-auto w-[80%] 2xl:w-[70%]">
-      <h2 className="text-center text-4xl font-bold md:text-6xl">
-        Received recommended languages
-      </h2>
-      <p className="mt-s2 mb-s4 text-center text-lg md:mx-auto md:w-2/5 md:text-xl">
-        We recommend you translate for these languages. Feel free to edit the
-        list as you please!
+    <div className="m-horizontal">
+      <h2 className="text-3xl md:text-center md:text-4xl">Select your plan</h2>
+      <p className="mt-4 mb-8 text-lg text-white/90 md:text-center md:text-[19px]">
+        Fees are waved upon channel becoming monetized.
       </p>
-      <div className="mx-auto grid grid-cols-1 justify-center gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {languages
-          .filter((el) => el !== userData.defaultLanguage)
-          .map(
-            (language, index) =>
-              language !== 'Others' && (
-                <div
-                  className="gradient-dark mx-auto flex w-full max-w-[360px] flex-row justify-between rounded-md p-s1.5"
-                  key={index}
-                >
-                  <div className="flex flex-row items-center justify-between">
-                    <Image
-                      src={youtubeChannel.channelDetails.thumbnail}
-                      alt="profile-image"
-                      height={40}
-                      width={40}
-                      className="block rounded-full"
-                    />
-                    <div className="ml-3 flex flex-col">
-                      <h2 className="text-lg">
-                        {userData.youtubeChannelName}{' '}
-                        {findLocalDialect(language)?.['localDialect']}
-                      </h2>
-                      <p className="text-sm">
-                        {findLocalDialect(language)?.['languageName']}
-                      </p>
-                    </div>
-                  </div>
-                  <button
-                    onClick={() =>
-                      handleRemoveLanguage(
-                        findLocalDialect(language)['languageName']
-                      )
-                    }
-                  >
-                    <Image src={Trash} alt="Delete" width={24} height={24} />
-                  </button>
-                </div>
-              )
-          )}
+      <div className="mb-10 flex w-full flex-wrap justify-center gap-8 px-4 md:px-0 xl:grid xl:grid-cols-3 xl:justify-between">
+        {plans.map((plan, i) => (
+          <div key={i}>
+            <Card borderRadius="xl" fullWidth={true}>
+              <PriceSection plan={plan} />
+            </Card>
+          </div>
+        ))}
       </div>
-      {isError && (
-        <p className="my-s3 text-center text-xl text-red">
-          At least one language is required
-        </p>
-      )}
-
-      {selectLanguages && (
-        <div className="mx-auto mt-4 max-w-[360px]">
-          <MultipleSelectInput
-            hideCheckmark
-            text="Edit suggested languages"
-            options={allLanguages}
-            answer={languages}
-            hasSubmitted={true}
-            onChange={(event) => handleMultipleLanguages(event)}
-          />
-        </div>
-      )}
-
-      <div className="mx-auto mt-4 w-[min(360px,90%)]">
-        <OnboardingButton onClick={handleSubmit} theme="light">
+      <div className="m-auto mt-12 w-[min(360px,90%)]">
+        <OnboardingButton
+          onClick={() => router.push('/onboarding?stage=6')}
+          theme="light"
+        >
           Continue
         </OnboardingButton>
       </div>
-      {!selectLanguages && (
-        <div className="mx-auto mt-4 w-[min(360px,90%)]">
-          <OnboardingButton
-            onClick={() => setSelectLanguages(true)}
-            theme="white"
-          >
-            Add another language
-          </OnboardingButton>
+    </div>
+  );
+};
+
+const PriceSection = ({ plan }) => {
+  const handlePlanSelect = () => {
+    localStorage.setItem('payForPlan', plan.id);
+  };
+
+  return (
+    <div
+      className={`relative h-full w-full cursor-pointer rounded-xl bg-white-transparent  px-4 py-8 text-white md:px-6 ${
+        plan.id === 'Pro'
+          ? 'gradient-dark border-0'
+          : 'border-xl border border-transparent'
+      }`}
+    >
+      <span className="rounded-md bg-gray-1 p-s1 pt-2.5 uppercase">
+        {plan.desc}
+      </span>
+      <p className="mb-2 mt-3 font-semibold">{plan.description}</p>
+
+      <div className="my-s4 flex flex-row items-center justify-start gap-x-4">
+        <p className="text-center text-4xl font-bold md:text-7xl">
+          {typeof (plan.monthlyCost || plan.yearlyCost) === 'number' && '$'}
+          {plan.monthlyCost != 'Free'
+            ? Math.round(plan.yearlyCost / 12)
+            : 'Free'}
+        </p>
+      </div>
+
+      {plan.options.map((option, index) => (
+        <div className="mt-s1.5 flex flex-row items-start gap-2" key={index}>
+          <Image
+            src={check}
+            alt="check-mark"
+            className="mt-2"
+            width={16}
+            height={16}
+          />
+          <p>{option}</p>
+        </div>
+      ))}
+
+      <div className="mt-s2 capitalize">
+        <Button
+          type={plan.id === 'pro' ? 'primary' : 'secondary'}
+          purpose="onClick"
+          onClick={handlePlanSelect}
+          fullWidth={true}
+        >
+          {'Go ' + plan.id}
+        </Button>
+      </div>
+
+      {plan.id == 'pro' && (
+        <div className="absolute top-4 left-1/2 -translate-x-1/2 -translate-y-full transform">
+          <Border borderRadius="3xl" padding="p-[1px]">
+            <div className="block rounded-3xl bg-white px-3 py-1 text-center font-medium text-black">
+              Recommended
+            </div>
+          </Border>
         </div>
       )}
     </div>
