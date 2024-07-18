76ba8a76b01feb110a88725bab435a39c636af75
d-huliu <huliud9vid@gmail.com>
2024-07-18 16:40:43 -0400

    one time cookie pop-up

--- components/sections/home/CookieConsent.js
+++ components/sections/home/CookieConsent.js
@@ -1,41 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import OnboardingButton from '../../Onboarding/button';
+import Cookies from 'js-cookie';
 
 const CookieConsent = () => {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    setShow(true);
+    // Check if the consent cookie is set
+    const consentGiven = Cookies.get('cookieConsent');
+    if (!consentGiven) {
+      setShow(true);
+    }
   }, []);
 
-  const handleClose = () => {
+  const handleContinue = () => {
+    // Set the consent cookie when the user presses continue
+    Cookies.set('cookieConsent', 'true', { expires: 7 }); // Expires in 7 days
     setShow(false);
   };
 
   if (!show) return null;
 
   return (
-    <button>
-      <div className="bg-black fixed bottom-5 w-8/12 left-1/2 -translate-x-1/2 flex justify-center rounded-3xl items-center z-50">
-        <div className="text-white bg-white-transparent text-center py-4 rounded-3xl flex justify-center items-center w-full">
-          <p className="mr-2">
-            By using this website, you agree to our{' '}
-            <a href="/terms-of-service" className="underline text-blue/80">use of cookies</a>. We use cookies to give you a great browsing experience.
-          </p>
-          <div className="">
-            <OnboardingButton
-              pb="pb-s0"
-              pt="pt-s0"
-              theme='light'
-              onClick={handleClose}
-              className="text-black px-2 rounded-lg"
-            >
-              Continue
-            </OnboardingButton>
-          </div>
+    <div className="bg-black fixed bottom-5 w-8/12 left-1/2 -translate-x-1/2 flex justify-center rounded-3xl items-center z-50">
+      <div className="text-white bg-white-transparent text-center py-2 rounded-3xl flex justify-center items-center w-full">
+        <p className="mr-2">
+          By using this website, you agree to our{' '}
+          <a href="/terms-of-service" className="underline text-blue/80">use of cookies</a>. We use cookies to give you a great browsing experience.
+        </p>
+        <div className="">
+          <OnboardingButton
+            pb="pb-s0"
+            pt="pt-s0"
+            theme='light'
+            onClick={handleContinue}
+            className="text-black px-2 rounded-lg"
+          >
+            Continue
+          </OnboardingButton>
         </div>
       </div>
-    </button>
+    </div>
   );
 };
 
